80e8fa6e0f0fd144e94d95712f6e9c7f139b06d0
Shaurya <shaurya.sighadia@plaksha.edu.in>
2022-01-27 18:10:21 +0530

    changed

--- GitNShit.py
+++ GitNShit.py
@@ -1,10 +1,12 @@
-#PALINDROM CODE
+#PALINDROME CODE
 
 pal = input('Enter a phrase of string')
 
 def isPalindrome(pal):
-    return pal == pal[::-1]
-
+    if type(pal)!=str:
+        raise TypeError("String only")
+    else:
+        return pal == pal[::-1]
 
 ans = isPalindrome(pal)
  
